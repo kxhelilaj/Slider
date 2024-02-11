7a5fe45c0318f8faa01ce3daf4ec85033c9a5aa6
kxhelilaj <kejvixh12@gmail.com>
2024-02-11 18:47:57 +0100

    inital commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  // state
+  const [time, setTime] = useState(8); // Initial time set to 8am
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // Reset function
+  const reset = () => {
+    setTime(8); // Reset time to 8am
   };
 
+  // renders
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <View style={styles.container}>
+          <Button
+            onPress={handlePresentModalPress}
+            title="Present Modal"
+            color="black"
+          />
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            index={1}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}
+          >
+            <View style={styles.contentContainer}>
+              <Button onPress={reset} title="Reset" />
+              <View style={styles.sliderContainer}>
+                <Text style={styles.timeText}>8:00 AM</Text>
+                <MultiSlider
+                  min={0}
+                  max={24}
+                  values={[24 - time]} // Reverse the value array
+                  markerOffsetX={24 - time}
+                  selectedStyle={{
+                    backgroundColor: '#EEF3F7'
+                  }}
+                  unselectedStyle={{
+                    backgroundColor: '#895CDF'
+                  }}
+                  onValuesChange={(newValues) => setTime(24 - newValues[0])} // Update time accordingly
+                />
+                <Text style={styles.timeText}>{time}</Text>
+              </View>
+            </View>
+          </BottomSheetModal>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  sliderContainer: {
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  timeText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
